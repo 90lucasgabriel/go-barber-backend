07cf630de42343c29a14842ea8d4037cbbac2f84
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-09 15:46:35 -0300

    13 - New: Test appointments at same time;

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentsService';
 
@@ -20,5 +22,26 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('12345678');
   });
 
-  // it('should not be able to create a new appointment at same time', () => {});
+  it('should not be able to create a new appointment at same time', async () => {
+    // Prepare
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    // Execute
+    const appointment = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345678',
+    });
+
+    // Assert
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12345678',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
