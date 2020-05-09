bd67f8c772780894f6ad3d7e77925ffec1e06a5f
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-09 14:08:06 -0300

    11 - New: CreateAppointment test; 11 - New: Fake repository; 11 - Update: jest and tsconfig;

--- src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,24 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentsService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    // Prepare
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    // Execute
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345678',
+    });
+
+    // Assert
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('12345678');
+  });
+
+  // it('should not be able to create a new appointment at same time', () => {});
 });
