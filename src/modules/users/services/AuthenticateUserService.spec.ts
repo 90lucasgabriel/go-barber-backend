5738bff8f4343e5e4a654d8bd1d3dba56b3474e2
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-09 18:26:08 -0300

    16 - New: Auth error tests;

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -23,13 +23,69 @@ describe('AuthenticateUser', () => {
       email: 'johndoe@email.com',
       password: '123456',
     };
+    const authData = {
+      email: 'johndoe@email.com',
+      password: '123456',
+    };
 
     // Act
     const user = await createUser.execute(userData);
-    const response = await authenticateUser.execute(userData);
+    const response = await authenticateUser.execute(authData);
 
     // Assert
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    // Arrange
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const userData = {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    };
+
+    // Act
+    const response = authenticateUser.execute(userData);
+
+    // Assert
+    expect(response).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    // Arrange
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const userData = {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    };
+    const authData = {
+      email: 'johndoe@email.com',
+      password: 'wrong-password',
+    };
+
+    // Act
+    await createUser.execute(userData);
+    const response = authenticateUser.execute(authData);
+
+    // Assert
+    expect(response).rejects.toBeInstanceOf(AppError);
   });
 });
