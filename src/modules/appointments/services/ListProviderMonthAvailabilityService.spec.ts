b1f49ffce886ef2a1bb9038598e6eed5f684cb86
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-16 16:16:52 -0300

    32 - New: List available days in month;

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -17,27 +17,22 @@ describe('ListProviderMonthAvailability', () => {
   it('should be able to list the month availability fro provider.', async () => {
     // Arrange
     const provider_id = uuid();
-    const appointmentData1 = {
+    for (let i = 8; i <= 17; i++) {
+      await fakeAppointmentsRepository.create({
+        provider_id,
+        date: new Date(2020, 4, 20, i, 0, 0),
+      });
+    }
+
+    await fakeAppointmentsRepository.create({
       provider_id,
-      date: new Date(2020, 3, 20, 8, 0, 0),
-    };
-    const appointmentData2 = {
-      provider_id,
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    };
-    const appointmentData3 = {
-      provider_id,
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    };
-    const appointmentData4 = {
+      date: new Date(2020, 3, 19, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
       provider_id,
       date: new Date(2020, 4, 21, 8, 0, 0),
-    };
-
-    await fakeAppointmentsRepository.create(appointmentData1);
-    await fakeAppointmentsRepository.create(appointmentData2);
-    await fakeAppointmentsRepository.create(appointmentData3);
-    await fakeAppointmentsRepository.create(appointmentData4);
+    });
 
     // Act
     const availability = await listProviderMonthAvailabilityService.execute({
@@ -49,10 +44,10 @@ describe('ListProviderMonthAvailability', () => {
     // Assert
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, availability: true },
-        { day: 20, availability: false },
-        { day: 21, availability: false },
-        { day: 22, availability: true },
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
