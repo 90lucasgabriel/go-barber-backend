dd09280be209b16adda478916abaeca2ec7dfc89
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-15 21:27:16 -0300

    30 - New: Providers list, routes, controller, repository, fake and test;

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProvidersService: ListProvidersService;
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProvidersService = new ListProvidersService(fakeUsersRepository);
+  });
+  it('should be able to list providers.', async () => {
+    // Arrange
+    const userData1 = {
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    };
+    const userData2 = {
+      name: 'John Tre',
+      email: 'johntre@email.com',
+      password: '123456',
+    };
+    const loggedUserData = {
+      name: 'John Qua',
+      email: 'johnqua@email.com',
+      password: '123456',
+    };
+
+    const user1 = await fakeUsersRepository.create(userData1);
+    const user2 = await fakeUsersRepository.create(userData2);
+    const loggedUser = await fakeUsersRepository.create(loggedUserData);
+
+    // Act
+    const providers = await listProvidersService.execute({
+      user_id: loggedUser.id,
+    });
+
+    // Assert
+    expect(providers).toEqual([user1, user2]);
+  });
+});
