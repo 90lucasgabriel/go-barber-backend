c30803e97e32bf2ad00de0ede15876542b6bcaac
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-18 11:44:10 -0300

    40 - New: Save notifications on MongoDB;

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,7 @@
 import { container } from 'tsyringe';
 
 import '@modules/users/providers';
-import './providers'
+import './providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -11,6 +11,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRespository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -24,5 +27,10 @@ container.registerSingleton<IUsersRepository>(
 
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
-  UserTokensRepository
-)
+  UserTokensRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
+);
