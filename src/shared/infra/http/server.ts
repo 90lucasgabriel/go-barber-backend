e4b1fc36d12b7c9962daf05cccf056498e6b3f52
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-18 15:36:49 -0300

    42 - New: Routes validation with celebrate;

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import uploadConfig from '@config/upload';
@@ -16,6 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uplaodsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, _: NextFunction) => {
