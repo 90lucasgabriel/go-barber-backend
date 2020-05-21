2b36d1cf8dc786b8cf3d75a550ca9977eea23fb0
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-21 00:41:19 -0300

    45 - New: Email Amazon SES;

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'dev@lucasgabriel.app',
+      name: 'Lucas do GoBarber',
+    },
+  },
+} as IMailConfig;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,13 +1,15 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealMailProvider from '@shared/container/providers/MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from '@shared/container/providers/MailProvider/implementations/SESMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -21,5 +23,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider),
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtherealMailProvider)
+    : container.resolve(SESMailProvider),
 );
