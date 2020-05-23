d836b49825f4464c3ec7edc91cbe3bef27a09065
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-23 16:54:31 -0300

    47 - New: Upload to Amazon S3;

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ import '@shared/container';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.uplaodsFolder));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
 app.use(errors());
