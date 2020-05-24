3866f02fbe56ede6dfc44396281a2d948fb02f74
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-23 21:51:51 -0300

    53 - New: Rate limit and brute force; 53 - New: Redis env config;

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,12 +8,15 @@ import { errors } from 'celebrate';
 
 import routes from './routes';
 import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
+
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
