659624e00cbeb1280fed1350265305d6a0665f3f
Lucas Teixeira <lucas.teixeira@webjump.com.br>
2020-05-27 21:15:07 -0300

    58 - Update: Cache serialization;

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
@@ -33,14 +34,16 @@ class ListProviderAppointmentsService {
     );
 
     if (!appointments) {
-      appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-        provider_id,
-        day,
-        month,
-        year,
-      });
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          day,
+          month,
+          year,
+        },
+      );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
